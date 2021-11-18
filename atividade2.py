9525ac5ee06a31d54b1487f0804ae3033076ac50
nauacbr <83404051+nauacbr@users.noreply.github.com>
2021-11-18 18:04:38 -0300

    Add files via upload

---
+++ atividade2.py
@@ -0,0 +1,4 @@
+# Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+n = input('qual seu número?')
+print('o numero informado foi', n)
---
+++ atividade5.py
@@ -0,0 +1,5 @@
+# Faça um Programa que converta metros para centímetros.
+
+m = float(input('digite a quantidade de metros'))
+cm = m*100
+print(cm)
