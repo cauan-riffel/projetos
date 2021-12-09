0afeb090e5c876e7cccb52c6c6dd380fc4f75778
nauacbr <83404051+nauacbr@users.noreply.github.com>
2021-12-08 23:44:15 -0300

    Delete atividade8.py

--- atividade8.py
+++
@@ -1,6 +0,0 @@
-#Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
-
-dht=float(input('quanto vovê recebe por hora?'))
-ht=float(input('quantas horas vovê trabalhou?'))
-dr=dht*ht
-print('você recebeu',dr)
