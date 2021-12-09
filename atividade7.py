e7b0fade0ff1264d8acc4ab4b295dd4375d9687a
nauacbr <83404051+nauacbr@users.noreply.github.com>
2021-12-08 23:41:36 -0300

    Delete atividade7.py

--- atividade7.py
+++
@@ -1,5 +0,0 @@
-#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
-
-b=float(input('digite a medida da base de seu quadrado'))
-a=(b*b)*2
-print('a area é',a,'cm²')
