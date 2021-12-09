f3b73da93655cc7f41bc90dffdb568d22a9266fd
nauacbr <83404051+nauacbr@users.noreply.github.com>
2021-12-08 23:44:13 -0300

    Delete atividade9.py

--- atividade9.py
+++
@@ -1,5 +0,0 @@
-# Faça um Programa que peça a temperatura em graus Farenheit, transforme e mostre a temperatura em graus Celsius.C = (5 * (F-32) / 9).
-
-F = float(input('quantos graus Farenheit estão agora?'))
-C = 5*(F-32)/9
-print('agora estão', C, 'graus Celsius')
