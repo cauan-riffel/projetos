8763481bd3e21f602ab502f41c0292487ad37231
nauacbr <83404051+nauacbr@users.noreply.github.com>
2021-12-08 23:40:15 -0300

    Delete atividade4.py

--- atividade4.py
+++
@@ -1,8 +0,0 @@
-# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
-
-nb1 = float(input('digite a primeira nota'))
-nb2 = float(input('digite a segunda nota'))
-nb3 = float(input('digite a terceira nota'))
-nb4 = float(input('digite a quarta nota'))
-r = (nb1+nb2+nb3+nb4)/4
-print(r)
