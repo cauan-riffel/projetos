8a231863766547a5b174add11c7bd025c3206c11
nauacbr <83404051+nauacbr@users.noreply.github.com>
2021-12-08 23:49:32 -0300

    Delete atividade12.py

--- atividade12.py
+++
@@ -1,5 +0,0 @@
-#Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
-
-H=float(input('digite sua altura em m'))
-pi=(72.7*H)-58
-print('seu peso ideal é',pi)
