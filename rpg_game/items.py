029ffabe7c20884bcd8b1cff1e3141633c8d537e
Nauac <83404051+nauacbr@users.noreply.github.com>
2023-05-13 15:36:34 -0300

    Update items.py

--- rpg_game/items.py
+++ rpg_game/items.py
@@ -1,14 +1,30 @@
 import random
 
 
-class Weapon:
-  def __init__(self,nome:str,dano:float,precisao:int) -> None:
-    self.name=nome
-    self.damage=dano
-    self.ocuracity=precisao
+weapon_names=('adaga','espada de mão','espadão','arco curvo','arco longo','mosquete')
 
-  def atacar(self)->float:
-    if random.randint(0,1000)<=self.ocuracity:
+class Weapon:
+  def __init__(self,name:str,damage:float,accuracy:int) -> None:
+    self.name=accuracy
+    self.damage=damage
+    self.accuracy=accuracy
+
+  def attack(self)->float:
+    if random.randint(0,1000)<=self.accuracy:
       return self.damage
     else:
       return 0
+
+def create_weapon(nome:str)->Weapon:
+  if nome=='adaga':
+    return Weapon(nome,66,910)
+  elif nome=='epada de mão':
+    return Weapon(nome,66,910)
+  elif nome=='espadão':
+    return Weapon(nome,66,910)
+  elif nome=='arco curvo':
+    return Weapon(nome,66,910)
+  elif nome=='arco longo':
+    return Weapon(nome,66,910)
+  else:
+    return Weapon(nome,66,910)
