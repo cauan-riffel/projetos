04ab27d5b911fd779b35dc210028a5748b0c17a8
Nauac <83404051+nauacbr@users.noreply.github.com>
2023-05-13 16:58:40 -0300

    Update map.py

--- rpg_game/map.py
+++ rpg_game/map.py
@@ -4,36 +4,35 @@ import random
 
 class Game_map:
   """
+  step:path
     map:
-        6
-      5  5
-      4  4 4
-      3  3 3
-      2   2
-      1   1
-        0
+           5:1
+      4:1      4:2
+      3:1   3:2  3:3
+      2:1      2:2
+      1:1      1:2
+           0:1
     """
-  def __init__(self,name:str,description:str,chances:list[int]=[25,100],step=0) -> None:
+  def __init__(self,name:str,description:str,chances:tuple[int]=(25,100),step=0,path=0)->None:
     self.step=step
+    self.path=path
     self.name=name
     self.desc=description
-    self.evet=self.do_event(chances)
-    if self.evet==1:
-      # self.gif=weapons(random.choice(weapon_names))
-      pass
-    elif self.evet==2:
-      # self.enemy=enemy(random.choice(enemys_names))
-      pass
+    self.event=self.do_event(chances)
+    self.gif=None
+    self.enemy=None
+    if self.event==1:
+      self.gif=create_weapon(random.choice(weapon_names))
+    elif self.event==2:
+      self.enemy=enemy_names(random.choice(enemy_names))
 
-
-
-  def show_local(self):
+  def show_local(self)->None:
     last_word=self.name.split(' ')[0][-1]
     end='o'if last_word=='o'else'a'
     print('você adentrou n%s %s'%(end, self.name))
     print(self.desc)
 
-  def do_event(self,chances)->int:
+  def do_event(self,chances:tuple[int])->int:
     temp=random.randint(0,100)
     if temp<=chances[0]:
       return 1
@@ -41,3 +40,10 @@ class Game_map:
       return 2
     else:
       return 0
+
+
+def create_map(step:str, path:str)->Game_map:
+  if path==1:
+    if step==0:
+      return Game_map()
+  else:
