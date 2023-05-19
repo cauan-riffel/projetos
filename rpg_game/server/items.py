70084a3d6aaa5d93a943536c95552be83c731e47
adriano-bomm <83406371+adriano-bomm@users.noreply.github.com>
2023-05-19 02:18:04 -0300

    Update items.py

--- rpg_game/server/items.py
+++ rpg_game/server/items.py
@@ -3,6 +3,7 @@ import random
 
 weapon_names=('adaga','espada de mão','espadão','arco curvo','arco longo','mosquete')
 
+
 class Weapon:
   def __init__(self,name:str,damage:float,accuracy:int)->None:
     self.name=name
@@ -10,11 +11,11 @@ class Weapon:
     self.accuracy=accuracy
 
   def show_description(self)->None:
-    return '%s:{dano:%.2f, precisão:%.1f}'%(self.name, self.damage, self.accuracy/10)
+    return '%s:{dano:%.2i, precisão:%.1f}'%(self.name,self.damage,self.accuracy/10)
 
 
 class Adaga(Weapon):
-  def __init__(self, name: str, damage: float, accuracy: int) -> None:
+  def __init__(self)->None:
     super().__init__('adaga',45,910)
 
   def attack(self)->float:
@@ -24,49 +25,52 @@ class Adaga(Weapon):
       return 0
 
 
-class Espada_de_Mao(Weapon):
-  def __init__(self, name: str, damage: float, accuracy: int) -> None:
+class Espada_de_mao(Weapon):
+  def __init__(self)->None:
     super().__init__("espada de mão", 135, 700)
+
+  def attack(self)->float:
+    if random.randint(0,1000)<=self.accuracy:
+      return self.damage
+    else:
+      return 0
+
+
+class Espadao(Weapon):
+  def __init__(self)->None:
+    super().__init__("Espadão", 210, 410)
+  
+  def attack(self)->float:
+    if random.randint(0,1000)<=self.accuracy:
+      return self.damage
+    else:
+      return 0
+
+
+class Arco_recurvo(Weapon):
+  def __init__(self)->None:
+    super().__init__("Arco Curvo", 250, 300)
     
+  def attack(self)->float:
+    if random.randint(0,1000)<=self.accuracy:
+      return self.damage
+    else:
+      return 0
+
+
+class Arco_longo(Weapon):
+  def __init__(self)->None:
+    super().__init__("Arco Longo", 280, 430)
+
   def attack(self) -> float:
     if random.randint(0,1000)<=self.accuracy:
       return self.damage
     else:
       return 0
 
-class Espadao(Weapon):
-  def __init__(self, name: str, damage: float, accuracy: int) -> None:
-    super().__init__("Espadão", 210, 410)
-  
-  def attack(self) -> float:
-    if random.randint(0,1000)<=self.accuracy:
-      return self.damage
-    else:
-      return 0
-    
-class Arco_Curvo(Weapon):
-  def __init__(self, name: str, damage: float, accuracy: int) -> None:
-    super().__init__("Arco Curvo", 250, 300)
-    
-  def attack(self) -> float:
-    if random.randint(0,1000)<=self.accuracy:
-      return self.damage
-    else:
-      return 0
-  
-class Arco_Longo(Weapon):
-  def __init__(self, name: str, damage: float, accuracy: int) -> None:
-    super().__init__("Arco Longo", 280, 430)
-    
-  def attack(self) -> float:
-    if random.randint(0,1000)<=self.accuracy:
-      return self.damage
-    else:
-      return 0
-      
-    
+
 class Mosquete(Weapon):
-  def __init__(self, name: str, damage: float, accuracy: int) -> None:
+  def __init__(self)->None:
     super().__init__('mosquete',400,230)
 
   def attack(self)->float:
