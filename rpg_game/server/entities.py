a29f90b96168d5fa5ebdd7207ef1ae792b0de663
Nauac <83404051+nauacbr@users.noreply.github.com>
2023-05-19 01:25:13 -0300

    Update entities.py

--- rpg_game/server/entities.py
+++ rpg_game/server/entities.py
@@ -1,4 +1,4 @@
-from items import Weapon,create_weapon
+from items import Weapon,Adaga,Mosquete
 
 class Character:
   def __init__(self,name:str,equipment:Weapon,life:int=200)->None:
@@ -21,8 +21,7 @@ class Character:
     return self.equipment.attack()
 
   def show_status(self):
-    print('nome: %s\nvida: %.2f/%.2f'%(self.name,self.life,self.MAX_LIFE))
-    self.equipment.show_description()
+    return 'nome: %s\nvida: %.2f/%.2f\ndescrição:%s'%(self.name,self.life,self.MAX_LIFE,self.equipment.show_description())
 
 
 enemy_names=('mercenario','esqueleto','elfo','elfo negro','ogro','Mazzutti')
@@ -33,6 +32,9 @@ class Enemy:
     self.life=life
     self.equipment=equipment 
     self.description=description
+    '''
+    receive_attack return True if this enemy is dead
+    '''
 
   def attack(self):
     return self.equipment.attack()
@@ -42,19 +44,27 @@ class Mercenario(Enemy):
     super().__init__('mercenario', 100, Adaga(), 'Este mercenário esta disposto a tudo para pilhar aqueles no seu caminho com sua adaga')
 
   def receive_attack(self,damage:int)->bool:
-    '''
-    return True if this enemy is dead
-    '''
     self.life-=damage
     if self.life<=0:
       return True
     else:
       return False
 
+
+
+class Mazzutti(Enemy):
+  def __init__(self)->None:
+    super().__init__('Mazzutti', 150, Mosquete(), 'um carecão lindo pelado com o mosquetão de fora vindo te pegar')
+
+    def receive_attack(self,damage:int)->bool:
+      self.life-=damage
+      if self.life<=0:
+        return True
+      else:
+        return False
+
 def create_enemy(name:str)->Enemy:
-  if name=='mercenario':
-    return Enemy(name,100,create_weapon('adaga'),'Este mercenário esta disposto a tudo para pilhar aqueles no seu caminho com sua adaga')
-  elif name=='esqueleto':
+  if name=='esqueleto':
     return Enemy(name,150,create_weapon('espada de mão'),'Um esqueleto humano sedento por carne de humanos que quando você menos espera, ataca você com sua espada de mão')
   elif name=='ogro':
     return Enemy(name,250,create_weapon('espadão'),'Um ser que usa de força bruta que utiliza um grande espadão')
@@ -62,5 +72,3 @@ def create_enemy(name:str)->Enemy:
     return Enemy(name,200,create_weapon('arco curvo'),'Um elfo arqueiro que usa apenas de sua habilidade para matar aqueles a sua volta utilizando um arco curvo')
   elif name=='elfo negro':
     return Enemy(name,250,create_weapon('arco longo'),'Um elfo negro arqueiro que mata todos aqueles em seu caminho utilizando um arco longo')
-  else:
-    return Enemy(name,5000,create_weapon('mosquete'),'um carecão lindo pelado com o mosquetão de fora vindo te pegar')
