fc141ed611bda421acc23e348de9b47b4fae7b6f
adriano-bomm <83406371+adriano-bomm@users.noreply.github.com>
2023-05-19 02:17:04 -0300

    Update entities.py

--- rpg_game/server/entities.py
+++ rpg_game/server/entities.py
@@ -1,4 +1,5 @@
-from items import Weapon,Adaga,Mosquete
+from items import Weapon,Adaga,Espada_de_mao,Mosquete,Espadao,Arco_longo,Arco_recurvo
+from server.items import Weapon
 
 class Character:
   def __init__(self,name:str,equipment:Weapon,life:int=200)->None:
@@ -23,7 +24,6 @@ class Character:
   def show_status(self):
     return 'nome: %s\nvida: %.2f/%.2f\ndescrição:%s'%(self.name,self.life,self.MAX_LIFE,self.equipment.show_description())
 
-
 enemy_names=('mercenario','esqueleto','elfo','elfo negro','ogro','Mazzutti')
 
 class Enemy:
@@ -40,8 +40,8 @@ class Enemy:
     return self.equipment.attack()
 
 class Mercenario(Enemy):
-  def __init__(self,name:str,life:int,equipment:Weapon,description:str)->None:
-    super().__init__('mercenario', 100, Adaga(), 'Este mercenário esta disposto a tudo para pilhar aqueles no seu caminho com sua adaga')
+  def __init__(self)->None:
+    super().__init__('mercenario',100,Adaga(),'Este mercenário esta disposto a tudo para pilhar aqueles no seu caminho com sua adaga')
 
   def receive_attack(self,damage:int)->bool:
     self.life-=damage
@@ -51,10 +51,57 @@ class Mercenario(Enemy):
       return False
 
 
+class Esqueleto(Enemy):
+  def __init__(self) -> None:
+    super().__init__("Esqueleto",150,Espada_de_mao(),"Um esqueleto  sedento por carne de humanos que quando você menos espera, ataca você com sua espada de mão")  
+
+  def receive_attack(self,damage:int)->bool:
+    self.life-=damage
+    if self.life<=0:
+      return True
+    else:
+      return False
+
+
+class Ogro(Enemy):
+  def __init__(self) -> None:
+    super().__init__("Ogro",250,Espadao(),"Um ser que usa de força bruta que utiliza um grande espadão")
+
+  def receive_attack(self,damage:int)->bool:
+    self.life-=damage
+    if self.life<=0:
+      return True
+    else:
+      return False
+
+
+class Elfo(Enemy):
+  def __init__(self) -> None:
+    super().__init__("Elfo",00, Arco_recurvo,"Um elfo arqueiro que usa apenas de sua habilidade para matar aqueles a sua volta utilizando um arco recurvo")
+
+  def receive_attack(self,damage:int)->bool:
+    self.life-=damage
+    if self.life<=0:
+      return True
+    else:
+      return False
+
+
+class Elfo_Negro(Enemy):
+  def __init__(self,) -> None:
+    super().__init__("Elfo Negro", 250, Arco_longo, "Um elfo negro arqueiro que mata todos aqueles em seu caminho utilizando um arco longo")
+
+  def receive_attack(self,damage:int)->bool:
+    self.life-=damage
+    if self.life<=0:
+      return True
+    else:
+      return False
+    
 
 class Mazzutti(Enemy):
   def __init__(self)->None:
-    super().__init__('Mazzutti', 150, Mosquete(), 'um carecão lindo pelado com o mosquetão de fora vindo te pegar')
+    super().__init__('Mazzutti',150,Mosquete(),'um carecão lindo pelado com o mosquetão de fora vindo te pegar')
 
     def receive_attack(self,damage:int)->bool:
       self.life-=damage
@@ -62,13 +109,3 @@ class Mazzutti(Enemy):
         return True
       else:
         return False
-
-def create_enemy(name:str)->Enemy:
-  if name=='esqueleto':
-    return Enemy(name,150,create_weapon('espada de mão'),'Um esqueleto humano sedento por carne de humanos que quando você menos espera, ataca você com sua espada de mão')
-  elif name=='ogro':
-    return Enemy(name,250,create_weapon('espadão'),'Um ser que usa de força bruta que utiliza um grande espadão')
-  elif name=='elfo':
-    return Enemy(name,200,create_weapon('arco curvo'),'Um elfo arqueiro que usa apenas de sua habilidade para matar aqueles a sua volta utilizando um arco curvo')
-  elif name=='elfo negro':
-    return Enemy(name,250,create_weapon('arco longo'),'Um elfo negro arqueiro que mata todos aqueles em seu caminho utilizando um arco longo')
